1677edd1adb20f9352bb2edb3db27036577af789
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2016-12-19 21:09:25 -0800

    commiting run file move.

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -3,18 +3,29 @@ import os
 
 HERE = os.path.dirname(__file__)
 
-def file_page(request):
-    imported_text = open(os.path.join(HERE, 'sample.html')).read()
-    return Response(imported_text)
-
 def index_page(request):
-    imported_text = open(os.path.join(HERE, 'ben-files', 'index.html')).read()
+    imported_text = open(os.path.join(HERE, 'static', 'ben-files', 'index.html')).read()
     return Response(imported_text)
 
-def home_page(request):
-    return Response("This is my first view!")
+def post_page(request):
+    imported_text = open(os.path.join(HERE, 'static', 'ben-files', 'post.html')).read()
+    return Response(imported_text)
+
+def about_page(request):
+    imported_text = open(os.path.join(HERE, 'static', 'ben-files', 'about.html')).read()
+    return Response(imported_text)
+
+def contact_page(request):
+    imported_text = open(os.path.join(HERE, 'static', 'ben-files', 'contact.html')).read()
+    return Response(imported_text)
+
+def new_post_page(request):
+    imported_text = open(os.path.join(HERE, 'static', 'ben-files', 'new_post.html')).read()
+    return Response(imported_text)
 
 def includeme(config):
-    config.add_view(home_page, route_name='home')
-    config.add_view(file_page, route_name='file_page')
-    config.add_view(index_page, route_name='index')
+    config.add_view(index_page, route_name='list')
+    config.add_view(post_page, route_name='detail')
+    config.add_view(about_page, route_name='about')
+    config.add_view(contact_page, route_name='update')
+    config.add_view(new_post_page, route_name='create')
