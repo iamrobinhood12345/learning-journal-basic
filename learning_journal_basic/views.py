7ca1ca65df48f289b686f291ab210c6f17e695e3
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2016-12-19 19:55:54 -0800

    commiting mockup blog pages, thanks to bootstrap.

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -7,9 +7,14 @@ def file_page(request):
     imported_text = open(os.path.join(HERE, 'sample.html')).read()
     return Response(imported_text)
 
+def index_page(request):
+    imported_text = open(os.path.join(HERE, 'ben-files', 'index.html')).read()
+    return Response(imported_text)
+
 def home_page(request):
     return Response("This is my first view!")
 
 def includeme(config):
     config.add_view(home_page, route_name='home')
     config.add_view(file_page, route_name='file_page')
+    config.add_view(index_page, route_name='index')
