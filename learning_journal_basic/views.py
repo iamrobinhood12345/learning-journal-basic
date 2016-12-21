8b867402747bcfb03e608191af1a8c8b363bd5af
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2016-12-20 17:44:40 -0800

    commiting jinja template for index page

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -6,22 +6,23 @@ import io
 HERE = os.path.dirname(__file__)
 
 ENTRIES = [
-    {"title": "Entry 1", "id": 1, "creation_date": "Dec 20, 2016",
-    "body": "I learned some stuff about some other stuff."},
-    {"title": "Entry 2", "id": 2, "creation_date": "Dec 20, 2016",
-    "body": "I learned some stuff about some other stuff."},
-    {"title": "Entry 3", "id": 3, "creation_date": "Dec 20, 2016",
-    "body": "I learned some stuff about some other stuff."}
+    {"title": "Learning Journal - Day 11", "title1":"Pitches and Tools", "id": 11, "creation_date": "Dec 19, 2016",
+     "body": "<p>Today I learned a good deal about my classmates. Each of us took turns pitching ideas for project week projects. I was very impressed with the creativity of my classmates. Several of their ideas seem like very good ones. I wish I could help out with all of them. Alas, decisions must be made, and we will eventually come to each work on one of a handful of projects. Such is life. We must choose decisively, and live with our choices for the rest of our days.</p><p>Avery and Patrick had awesome presentations. I learned about Itertools from Patrick, and can't wait for the chance to practice. Avery presented on an enhancement for Visual Studio that allows you to see documentation for functions as you are writing them. How cool is that!</p>"},
+    {"title": "Entry 2", "title1": "New stuff learned","id": 2, "creation_date": "Dec 20, 2016",
+     "body": "I learned some stuff about some other stuff."},
+    {"title": "Entry 3", "title1": "New stuff learned", "id": 3, "creation_date": "Dec 20, 2016",
+     "body": "I learned some stuff about some other stuff."}
 ]
+
 
 @view_config(route_name='list', renderer='templates/list.jinja2')
 def index_page(request):
     # imported_text = open(os.path.join(HERE, 'static', 'ben-files', 'index.html')).read()
     # return imported_text
-    all_stuff = ['lskdjf', 'sldkfjslfdj', 'lskdjflsjdf']
-    return {"bag_list": all_stuff}
+    return {"ENTRIES": ENTRIES}
 
-@view_config(route_name='detail', renderer='string')
+
+@view_config(route_name='detail', renderer='templates/post_tenplate.jinja2')
 def post_page(request):
     # imported_text = open(os.path.join(HERE, 'static', 'ben-files', 'post.html')).read()
     # return Response(imported_text)
@@ -29,15 +30,18 @@ def post_page(request):
     entry = ENTRIES[the_id]
     return {"entry": entry}
 
+
 @view_config(route_name='about', renderer='string')
 def about_page(request):
     imported_text = open(os.path.join(HERE, 'static', 'ben-files', 'about.html')).read()
     return Response(imported_text)
 
+
 @view_config(route_name='update', renderer='string')
 def update_page(request):
     imported_text = open(os.path.join(HERE, 'static', 'ben-files', 'contact.html')).read()
     return Response(imported_text)
+
 
 @view_config(route_name='create', renderer='string')
 def new_post_page(request):
